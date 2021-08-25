6ce24e1bd956c9dc93423b66633f91da000f2a4b
Kshamav123 <Kshamav123>
2021-08-25 22:15:28 +0530

    Finding the denominations

---
+++ VendingMachine.java
@@ -0,0 +1,31 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+	public static void vendingMachine()
+    {
+        int[] notes = new int[]{ 1000, 500, 100, 50, 10, 5, 2, 1 };
+        int[] noteCounter = new int[8];
+        Scanner sc=new Scanner(System.in);
+       
+        System.out.println("Enter the amount");
+        int amount =sc.nextInt();
+        for (int i = 0; i < 8; i++) {
+            if (amount >= notes[i]) {
+                noteCounter[i] = amount / notes[i];
+                amount = amount - noteCounter[i] * notes[i];
+            }
+        }
+        sc.close();
+      
+        
+        System.out.println("Currency Count ->");
+        for (int i = 0; i < 8; i++) {
+            if (noteCounter[i] != 0) {
+                System.out.println(notes[i] + " : " + noteCounter[i]);
+                   
+            }
+        }
+    }
+}
