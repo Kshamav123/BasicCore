22b599fd121fba8f3f72e881aefde69b32542dda
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-27 07:25:29 +0530

    Updating the main method

--- maincore.java
+++ maincore.java
@@ -4,15 +4,32 @@ import com.basiccore.*;
 public class maincore {
 	public static void main(String[] args) {
 		System.out.println("Welcome to basic java main");
-		FlipCoin.flipCoin();
-		LeapYr.leapYr();
-		Power.power();
-		HarmonicNo.harmonicNo();
-		factors.factorNo();
-		quoRem.quorem();
-		SwapNo.swapNo();
-		EvenOdd.evenOdd();
-		Alphabet.alphabetVC();
+		// FlipCoin.flipCoin();
+		// LeapYr.leapYr();
+		// Power.power();
+		// HarmonicNo.harmonicNo();
+		// factors.factorNo();
+		// quoRem.quorem();
+		// SwapNo.swapNo();
+		// EvenOdd.evenOdd();
+		// Alphabet.alphabetVC();
+//     Quadratic.quadraticRoots(0, 0, 0);
+//		Distance.distance(0, 0);
+//		  WindChill.windChill(0, 0); 
+//		SumOfThree.create(null, 0); 
+//		  Fibonacci.fibonacci();		  
+//		 TempConv.temp();
+//		ReverseNo.reverseNo();
+//		MonthlyPay.monthlyPay();
+//		VendingMachine.vendingMachine();
+//		PrimeNo.primeNo();
+//		PerfectNo.perfectNo();
+//		  NewtonsSqrt.sqRt();
+//		Coupon.getCoupon();
+//	Gcalendar.calendar();
+//		  Binary.toBinary(10);
+//		  Nibble.nBinary();
+		  
 		
 	}
 }
