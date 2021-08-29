e10795e5bb93463b4592b689b83a30a252c97243
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-29 12:42:02 +0530

    To display the elapsed time

---
+++ StopWatch.java
@@ -0,0 +1,33 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class StopWatch {
+	public static void stopWatch() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Start Time in Hrs:Min:Sec format: ");
+		int hr1 = sc.nextInt();
+		int min1 = sc.nextInt();
+		int sec1 = sc.nextInt();
+		System.out.println("Enter Stop Time in Hrs:Min:Sec format: ");
+		int hr2 = sc.nextInt();
+		int min2 = sc.nextInt();
+		int sec2 = sc.nextInt();
+
+		if ((hr1 > 24 || hr2 > 24) || (min1 > 60 || min2 > 60) || (sec1 > 60 || sec2 > 60))
+			System.out.println("Invalid case. Please Enter proper timings.");
+		else {
+
+			long differenceInSec = (long) (Math.abs(sec2 - sec1));
+			long differenceInMin = (long) ((Math.abs(min2 - min1)));
+			long differenceInHrs = (long) ((Math.abs(hr2 - hr1)));
+
+			System.out.println("Elapsed Time (Hrs:Min:Sec): ");
+			System.out.println(differenceInHrs + ":" + differenceInMin + ":" + differenceInSec);
+
+		}
+
+	}
+
+}
