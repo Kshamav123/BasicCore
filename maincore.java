405771a4f5310b87104c938f2ff805293dc907c9
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-29 12:13:46 +0530

    Updating the main file

--- maincore.java
+++ maincore.java
@@ -1,19 +1,22 @@
 package com.basicjava;
+import com.logical.*;
 import com.basiccore.*;
+import com.logical.*;
 
 public class maincore {
 	public static void main(String[] args) {
-		System.out.println("Welcome to basic java main");
-		// FlipCoin.flipCoin();
-		// LeapYr.leapYr();
-		// Power.power();
-		// HarmonicNo.harmonicNo();
-		// factors.factorNo();
-		// quoRem.quorem();
-		// SwapNo.swapNo();
-		// EvenOdd.evenOdd();
-		// Alphabet.alphabetVC();
-//     Quadratic.quadraticRoots(0, 0, 0);
+		
+		  System.out.println("Welcome to basic java main"); 
+//		FlipCoin.flipCoin();
+//		  LeapYr.leapYr(); 
+//		  Power.power(); 
+//		HarmonicNo.harmonicNo(); 
+//		factors.factorNo();
+//		  quoRem.quorem(); 
+//		SwapNo.swapNo();
+//		EvenOdd.evenOdd(); 
+//		Alphabet.alphabetVC();
+//		  Quadratic.quadraticRoots(0, 0, 0);
 //		Distance.distance(0, 0);
 //		  WindChill.windChill(0, 0); 
 //		SumOfThree.create(null, 0); 
@@ -29,7 +32,10 @@ public class maincore {
 //	Gcalendar.calendar();
 //		  Binary.toBinary(10);
 //		  Nibble.nBinary();
+//       LargestNo.largestNo();	  
+//		  StopWatch.stopWatch();
 		  
-		
+		  
 	}
+	
 }
